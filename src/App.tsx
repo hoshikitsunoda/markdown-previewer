da9c7c015ce08b9ef0daf37d712858ce7cfb33ed
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-09-08 14:47:59 -0700

    setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
 function App() {
-  return <div className="App">Markdown Previewer</div>
+  return (
+    <div className="App">
+      Markdown Previewer
+      <input
+        className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+        type="email"
+        placeholder="jane@example.com"
+      />
+    </div>
+  )
 }
 
 export default App
