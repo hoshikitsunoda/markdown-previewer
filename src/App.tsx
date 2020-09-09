c81de8bb7c0eacbe909356cbb2b5978de320834e
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-09-09 00:28:34 -0700

    initiated TextArea component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 
+import TextArea from './TextArea'
+
 function App() {
   return (
-    <div className="App">
-      Markdown Previewer
-      <input
-        className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
-        type="email"
-        placeholder="jane@example.com"
-      />
+    <div className="App flex justify-center">
+      <TextArea />
+      <TextArea />
     </div>
   )
 }
---
+++ src/TextArea.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const TextArea = () => {
+  return (
+    <>
+      <textarea
+        className="flex-1 border border-gray-600"
+        name="textarea"
+        cols={30}
+        rows={10}
+      ></textarea>
+    </>
+  )
+}
+
+export default TextArea
